6f85f7772c14b16440bf732d61ef794a04f76a57
Devin Lorenz <devinlorenz.work@gmail.com>
2022-08-02 17:56:39 -0400

    First commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+require('dotenv').config();
+
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/home.html'));
+})
+
+
+const PORT = process.env.PORT || 4523;
+
+app.listen(PORT, () => {
+    console.log(`Server is jamming on port ${PORT}`);
+});
